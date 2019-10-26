0950127784b77f13afdfdcb17385c3b14f597adc
Solda-git <soldatovsl@mail.ru>
2019-10-26 18:37:36 +0300

    Файл инициализации

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .core import CinemaParser
+#__all__ = ["extract_raw_content", "print_raw_content", "get_films_list", "get_film_nearest_session", "get_nearest_session", "film_list"]
